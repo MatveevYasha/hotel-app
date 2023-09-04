74b4a698338884cb09573da41178c4a2df9de498
MatveevYasha <zif666ne@gmail.com>
2023-09-04 22:03:06 +0400

    Add text field validation

--- lib/ui/booking_page/booking_page.dart
+++ lib/ui/booking_page/booking_page.dart
@@ -15,6 +15,8 @@ import 'widgets/custom_text_row_widget.dart';
 import 'widgets/small_button_widget.dart';
 import 'widgets/tourist_widget.dart';
 
+final _formKey = GlobalKey<FormState>();
+
 class BookingPage extends ConsumerStatefulWidget {
   const BookingPage({super.key});
 
@@ -26,16 +28,11 @@ class _BookingPageConsumerState extends ConsumerState<BookingPage> {
   List<int> listTourists = [1];
   late int _totalPrice;
 
-  MaskTextInputFormatter maskNumberFormatter = MaskTextInputFormatter(
-    mask: '+7 (###) ###-##-##',
-    filter: {"#": RegExp(r'[0-9]')},
-    type: MaskAutoCompletionType.lazy,
-  );
-
-  MaskTextInputFormatter maskEmailFormatter = MaskTextInputFormatter(
-    filter: {"#": RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$')},
-    type: MaskAutoCompletionType.lazy,
-  );
+  // MaskTextInputFormatter maskNumberFormatter = MaskTextInputFormatter(
+  //   mask: '+7 (###) ###-##-##',
+  //   filter: {"#": RegExp(r'[0-9]')},
+  //   type: MaskAutoCompletionType.lazy,
+  // );
 
   @override
   Widget build(BuildContext context) {
@@ -64,152 +61,161 @@ class _BookingPageConsumerState extends ConsumerState<BookingPage> {
     return Scaffold(
       appBar: const HotelAppBar(title: 'Бронирование'),
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            const SizedBox(height: 8),
-            const LayoutWidget(
-              child: BaseInformationAboutHotel(),
-            ),
-            const SizedBox(height: 8),
-            LayoutWidget(
-              child: Column(
-                children: [
-                  CustomTextRowWidget(
-                    leftText: 'Вылет из',
-                    rightText: state.booking.departure,
-                    removeTopPadding: true,
-                  ),
-                  CustomTextRowWidget(
-                    leftText: 'Страна, город',
-                    rightText: state.booking.arrivalCountry,
-                  ),
-                  CustomTextRowWidget(
-                    leftText: 'Даты',
-                    rightText: '${state.booking.tourDateStart} – ${state.booking.tourDateStop}',
-                  ),
-                  CustomTextRowWidget(
-                    leftText: 'Кол-во ночей',
-                    rightText: '${state.booking.numberOfNights} ночей',
-                  ),
-                  const CustomTextRowWidget(
-                    leftText: 'Отель',
-                    rightText: 'Steigenberger Makadi',
-                  ),
-                  CustomTextRowWidget(
-                    leftText: 'Номер',
-                    rightText: state.booking.room,
-                  ),
-                  CustomTextRowWidget(
-                    leftText: 'Питание',
-                    rightText: state.booking.nutrition,
-                    removeBottomPadding: true,
-                  ),
-                ],
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              const SizedBox(height: 8),
+              const LayoutWidget(
+                child: BaseInformationAboutHotel(),
               ),
-            ),
-            const SizedBox(height: 8),
-            LayoutWidget(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Информация о покупателе',
-                    style: textTheme.bodyMedium,
-                  ),
-                  CustomTextFieldWidget(
-                    mask: maskNumberFormatter,
-                    keyboardType: TextInputType.phone,
-                    labelText: 'Номер телефона',
-                  ),
-                  const SizedBox(height: 8),
-                  CustomTextFieldWidget(
-                    // TODO: посмотреть другую валидацию
-                    mask: maskEmailFormatter,
-                    keyboardType: TextInputType.emailAddress,
-                    labelText: 'Почта',
-                  ),
-                  Text(
-                    'Эти данные никому не передаются. После оплаты мы вышли чек на указанный вами номер и почту',
-                    style: textTheme.bodySmall?.copyWith(
-                      fontSize: 14,
-                      color: const Color(0xFF828796),
+              const SizedBox(height: 8),
+              LayoutWidget(
+                child: Column(
+                  children: [
+                    CustomTextRowWidget(
+                      leftText: 'Вылет из',
+                      rightText: state.booking.departure,
+                      removeTopPadding: true,
                     ),
-                  ),
-                ],
+                    CustomTextRowWidget(
+                      leftText: 'Страна, город',
+                      rightText: state.booking.arrivalCountry,
+                    ),
+                    CustomTextRowWidget(
+                      leftText: 'Даты',
+                      rightText: '${state.booking.tourDateStart} – ${state.booking.tourDateStop}',
+                    ),
+                    CustomTextRowWidget(
+                      leftText: 'Кол-во ночей',
+                      rightText: '${state.booking.numberOfNights} ночей',
+                    ),
+                    const CustomTextRowWidget(
+                      leftText: 'Отель',
+                      rightText: 'Steigenberger Makadi',
+                    ),
+                    CustomTextRowWidget(
+                      leftText: 'Номер',
+                      rightText: state.booking.room,
+                    ),
+                    CustomTextRowWidget(
+                      leftText: 'Питание',
+                      rightText: state.booking.nutrition,
+                      removeBottomPadding: true,
+                    ),
+                  ],
+                ),
               ),
-            ),
-            const SizedBox(height: 8),
-            // TODO: сделать что нибудь со скролом (посмотреть видео может быть)
-            ListView.builder(
-              shrinkWrap: true,
-              itemCount: listTourists.length,
-              itemBuilder: (context, index) {
-                return TouristWidget(
-                  index: index,
-                );
-              },
-            ),
-            LayoutWidget(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Добавить туриста',
-                    style: textTheme.bodyMedium,
-                  ),
-                  SmallButtonWidget(
-                    onTap: () {
-                      setState(() {
-                        listTourists.add(1);
-                      });
-                    },
-                    icon: Icons.add,
-                    fill: true,
-                  )
-                ],
+              const SizedBox(height: 8),
+              LayoutWidget(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Информация о покупателе',
+                      style: textTheme.bodyMedium,
+                    ),
+                    CustomTextFieldWidget(
+                      // mask: maskNumberFormatter,
+                      keyboardType: TextInputType.phone,
+                      labelText: 'Номер телефона',
+                    ),
+                    const SizedBox(height: 8),
+                    CustomTextFieldWidget(
+                      // TODO: посмотреть другую валидацию
+                      // mask: maskEmailFormatter,
+                      keyboardType: TextInputType.emailAddress,
+                      labelText: 'Почта',
+                    ),
+                    Text(
+                      'Эти данные никому не передаются. После оплаты мы вышли чек на указанный вами номер и почту',
+                      style: textTheme.bodySmall?.copyWith(
+                        fontSize: 14,
+                        color: const Color(0xFF828796),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            const SizedBox(height: 8),
-            LayoutWidget(
-              child: Column(
-                children: [
-                  // TODO: шрифт посмотреть что точно правильный
-                  CustomTextCostRowWidget(
-                    leftText: 'Тур',
-                    // TODO: разделить цену
-                    rightText: '${state.booking.tourPrice} ₽',
-                    removeTopPadding: true,
-                  ),
-                  CustomTextCostRowWidget(
-                    leftText: 'Топливный сбор',
-                    rightText: '${state.booking.fuelCharge} ₽',
-                  ),
-                  CustomTextCostRowWidget(
-                    leftText: 'Сервисный сбор',
-                    rightText: '${state.booking.serviceCharge} ₽',
-                  ),
-                  CustomTextCostRowWidget(
-                    leftText: 'К оплате',
-                    rightText: '$_totalPrice',
-                    removeBottomPadding: true,
-                    costTextIsAccent: true,
-                  ),
-                ],
+              const SizedBox(height: 8),
+              // TODO: сделать что нибудь со скролом (посмотреть видео может быть)
+              ListView.builder(
+                shrinkWrap: true,
+                itemCount: listTourists.length,
+                itemBuilder: (context, index) {
+                  return TouristWidget(
+                    index: index,
+                  );
+                },
               ),
-            ),
-            const SizedBox(height: 8),
-          ],
+              LayoutWidget(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Добавить туриста',
+                      style: textTheme.bodyMedium,
+                    ),
+                    SmallButtonWidget(
+                      onTap: () {
+                        setState(() {
+                          listTourists.add(1);
+                        });
+                      },
+                      icon: Icons.add,
+                      fill: true,
+                    )
+                  ],
+                ),
+              ),
+              const SizedBox(height: 8),
+              LayoutWidget(
+                child: Column(
+                  children: [
+                    // TODO: шрифт посмотреть что точно правильный
+                    CustomTextCostRowWidget(
+                      leftText: 'Тур',
+                      // TODO: разделить цену
+                      rightText: '${state.booking.tourPrice} ₽',
+                      removeTopPadding: true,
+                    ),
+                    CustomTextCostRowWidget(
+                      leftText: 'Топливный сбор',
+                      rightText: '${state.booking.fuelCharge} ₽',
+                    ),
+                    CustomTextCostRowWidget(
+                      leftText: 'Сервисный сбор',
+                      rightText: '${state.booking.serviceCharge} ₽',
+                    ),
+                    CustomTextCostRowWidget(
+                      leftText: 'К оплате',
+                      rightText: '$_totalPrice',
+                      removeBottomPadding: true,
+                      costTextIsAccent: true,
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 8),
+            ],
+          ),
         ),
       ),
       bottomNavigationBar: BottomNavigationBlock(
         // TODO: разделить цену
         title: 'Оплатить $_totalPrice ₽',
         onTap: () {
-          Navigator.of(context).push(
-            MaterialPageRoute(
-              builder: (context) => const SuccessPage(),
-            ),
-          );
+          if (_formKey.currentState!.validate()) {
+            print('success');
+          } else {
+            print('NOT success!!!');
+          }
+
+          // Navigator.of(context).push(
+          //   MaterialPageRoute(
+          //     builder: (context) => const SuccessPage(),
+          //   ),
+          // );
         },
       ),
     );
--- lib/ui/booking_page/widgets/custom_text_field_widget.dart
+++ lib/ui/booking_page/widgets/custom_text_field_widget.dart
@@ -4,13 +4,13 @@ import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
 import '../../theme/text_theme.dart';
 
 class CustomTextFieldWidget extends StatefulWidget {
-  final MaskTextInputFormatter mask;
+  // final MaskTextInputFormatter mask;
   final TextInputType? keyboardType;
   final String labelText;
 
   const CustomTextFieldWidget({
     super.key,
-    required this.mask,
+    // this.mask,
     required this.labelText,
     this.keyboardType,
   });
@@ -20,8 +20,15 @@ class CustomTextFieldWidget extends StatefulWidget {
 }
 
 class _CustomTextFieldWidgetState extends State<CustomTextFieldWidget> {
+  MaskTextInputFormatter maskNumberFormatter = MaskTextInputFormatter(
+    mask: '+7 (###) ###-##-##',
+    filter: {"#": RegExp(r'[0-9]')},
+    type: MaskAutoCompletionType.lazy,
+  );
+
   FocusNode focusNode = FocusNode();
   final TextEditingController _controller = TextEditingController();
+  bool _isValid = true;
 
   @override
   void dispose() {
@@ -32,6 +39,38 @@ class _CustomTextFieldWidgetState extends State<CustomTextFieldWidget> {
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          setState(() {
+            _isValid = false;
+          });
+
+          return '';
+        }
+
+        if (widget.keyboardType == TextInputType.phone) {
+          if (value.length != 18 || value.contains(RegExp(r'[*]'))) {
+            setState(() {
+              _isValid = false;
+            });
+            return '';
+          }
+        }
+
+        if (widget.keyboardType == TextInputType.emailAddress) {
+          if (!value.contains(RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+"))) {
+            setState(() {
+              _isValid = false;
+            });
+            return '';
+          }
+        }
+
+        setState(() {
+          _isValid = true;
+        });
+        return null;
+      },
       controller: _controller,
       focusNode: focusNode,
       keyboardType: widget.keyboardType,
@@ -57,7 +96,7 @@ class _CustomTextFieldWidgetState extends State<CustomTextFieldWidget> {
           };
         }
       },
-      inputFormatters: [widget.mask],
+      inputFormatters: (widget.keyboardType == TextInputType.phone) ? [maskNumberFormatter] : null,
       decoration: InputDecoration(
         labelStyle: textTheme.bodySmall?.copyWith(
           color: const Color(0xFF828796),
@@ -65,7 +104,7 @@ class _CustomTextFieldWidgetState extends State<CustomTextFieldWidget> {
         ),
         labelText: widget.labelText,
         filled: true,
-        fillColor: const Color(0xFFF6F6F9),
+        fillColor: _isValid ? const Color(0xFFF6F6F9) : const Color(0xFFEB5757).withOpacity(0.15),
         focusedBorder: UnderlineInputBorder(
           borderRadius: BorderRadius.circular(10),
           borderSide: const BorderSide(
@@ -76,6 +115,12 @@ class _CustomTextFieldWidgetState extends State<CustomTextFieldWidget> {
           borderRadius: BorderRadius.circular(10),
           borderSide: const BorderSide(
             color: Color(0xFFF6F6F9),
+          ),
+        ),
+        errorBorder: UnderlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: BorderSide(
+            color: const Color(0xFFEB5757).withOpacity(0.15),
           ),
         ),
       ),
